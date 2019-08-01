d616650325cb409aac5c1ba67a46df0de2ecf5ac
KrzysztofNowacki <krzysztof.nowacki06@gmail.com>
2019-08-01 15:57:50 +0200

    Initial Commit

---
+++ 2048/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  2048
+//
+//  Created by Krzysiu on 01/08/2019.
+//  Copyright © 2019 k.now. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
